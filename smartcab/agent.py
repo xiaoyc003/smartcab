fb8f213065abe10255c61d400300ac73a53838b0
xiaoyc <xiaoyc003@126.com>
2018-01-21 16:58:13 +0800

    first commit

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -42,14 +42,14 @@ class LearningAgent(Agent):
         # If 'testing' is True, set epsilon and alpha to 0
         
         if testing == True:
-        	self.epsilon = 0.0
+            self.alpha = 0.0 
+            self.epsilon = 0.0
+            #self.alpha = 0.0
         else:
         	# self.epsilon -= 0.05  # Update epsilon
-        	self.epsilon = math.exp(-0.006 * self.trial) #0.006-500,0.015-200
+        	self.epsilon = math.exp(-0.006 * self.trial) 
 
         	self.trial += 1
-        	#self.epsilon = 1.0 / (self.trial ** 2)
-        	#self.epsilon = 0.9 ** self.trial
 
         return None
 
@@ -203,7 +203,7 @@ def run():
     #   display      - set to False to disable the GUI if PyGame is enabled
     #   log_metrics  - set to True to log trial and simulation results to /logs
     #   optimized    - set to True to change the default log file name
-    sim = Simulator(env,update_delay=0.001,log_metrics=True,optimized=True)
+    sim = Simulator(env,update_delay=0.001,display=False,log_metrics=True,optimized=True)
     
     ##############
     # Run the simulator
